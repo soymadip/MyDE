890f57501b967c7ba4c71baa00a643160ef91161
soymadip <soumadip@zohomail.in>
2025-10-25 00:56:59 +0530

    feat(myctl): implement setup wob-daemon subcommand

---
+++ dots/.local/lib/myctl/wob-osd.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Show a wob-style on-screen display (OSD) bar for levels
+
+# Check If wob daemon is running; if not, start it
+start-wob-daemon() {
+    local wob_pipe="/tmp/${HYPRLAND_INSTANCE_SIGNATURE:-myde_$(date +%s)}.wob"
+
+    log.debug "Wob pipe: '$wob_pipe'"
+
+    if [ -e "$wob_pipe" ] && [ ! -p "$wob_pipe" ]; then
+        log.error "$wob_pipe exists but is not a FIFO pipe!"
+        rm "$wob_pipe" && mkfifo "$wob_pipe"
+    elif [ ! -p "$wob_pipe" ]; then
+        log.info "Creating FIFO pipe at $wob_pipe"
+        mkfifo "$wob_pipe"
+    fi
+
+    # Check if the daemon is already running
+    pgrep -f "tail -f $wob_pipe.*wob" >/dev/null && {
+        log.info "Wob daemon is already running."
+        return 0
+    }
+
+    # Launch the daemon
+    log.debug "Starting wob daemon... "
+
+    if command -v uwsm >/dev/null; then
+        log.debug "Launching wob via uwsm"
+        uwsm app -- bash -c "tail -f $wob_pipe | wob & disown"
+    else
+        log.debug "Launching wob directly"
+        bash -c "tail -f $wob_pipe | wob & disown"
+    fi
+}
+#---------------------------------------------------------------------------
+
+# if executed directly,
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  echo "this is part of myctl controller program lib."
+  echo "use 'myctl help' for more info."
+fi
