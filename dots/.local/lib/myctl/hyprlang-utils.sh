20e74699be5edc780f52ee3bb9c1beeab5a179dd
soymadip <soumadip@zohomail.in>
2025-10-27 12:03:11 +0530

    feat(myctl): add basic hyprlang parser

---
+++ dots/.local/lib/myctl/hyprlang-utils.sh
@@ -0,0 +1,50 @@
+
+
+#==> myctl get hconf <key> [filename]
+#    defaults to: ~/.config/myde/myde.conf
+
+# Usage: get-hconf <key_name> <hypr_file>
+# Limitation: Doesn't expand hyprlang vars. only shell vars/cmds.
+# TODO:
+#       default file support
+#       default value support
+get-hconf() {
+    local key_name hypr_file raw_value final_value
+
+    [[ -z "$1" ]] && {
+        log.error "Error: No key name provided." >&2
+        return 1
+    }
+
+    [[ -z "$2" ]] && {
+        log.error "Error: No file name provided." >&2
+        return 1
+    }
+
+    key_name=$1
+    hypr_file="$2"
+
+    [[ ! -f "$hypr_file" ]] && {
+        log.error "Error: Config file not found at $hypr_file" >&2
+        return 1
+    }
+
+    # Find the key & Extract Value
+    raw_value=$(awk -F'=' -v key="$key_name" '
+      $0 ~ key && /^\s*\$/ {
+        sub(/#.*/, "", $2) # Remove comments from the value
+        print $2           # Print the value
+      }
+    ' "$hypr_file" | xargs)
+
+    [[ -z "$raw_value" ]] && {
+        log.error "Error: Key not found: $key_name" >&2
+        return 1
+    }
+
+    # Expand Value
+    final_value=$(eval echo "$raw_value")
+
+    # Return Result
+    echo "$final_value"
+}
