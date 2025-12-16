8eaee8f61f7b12077a4a3cb860227c85783aaadb
Soumadip Das <soumadip@zohomail.in>
2025-12-17 01:17:25 +0530

    feat(wezterm): add tabbar theming

--- dots/.config/wezterm/wezterm.lua
+++ dots/.config/wezterm/wezterm.lua
@@ -5,14 +5,33 @@ local config = wezterm.config_builder()
 
 local keybinds = require("keybinds")
 
+-------------------- Theme Overrides --------------------------
+
+local colors = {
+	accent = "#C98690",
+	bg = "#1E2030",
+}
+
+local theme = wezterm.color.get_builtin_schemes()["Catppuccin Macchiato"]
+
+theme.tab_bar.active_tab.bg_color = colors.accent
+
+theme.cursor_bg = colors.accent
+theme.cursor_fg = colors.accent
+theme.cursor_border = colors.accent
+
+theme.selection_fg = colors.bg
+theme.selection_bg = colors.accent
+
 --
 ----------------------- CONFIGS --------------------------
 
 local conf = {
-	------------- window Apperance -----------------
+	------------- window Appearance -----------------
 	max_fps = 144,
 	window_decorations = "TITLE | RESIZE",
-	color_scheme = "Catppuccin Macchiato (Gogh)",
+
+	color_scheme = "Catppuccin Macchiato Mocha",
 
 	window_close_confirmation = "AlwaysPrompt",
 	skip_close_confirmation_for_processes_named = {
@@ -29,6 +48,8 @@ local conf = {
 		top = 5,
 		bottom = 5,
 	},
+
+	color_schemes = { ["Catppuccin Macchiato Mocha"] = theme },
 
 	------------- Font Settings --------------------
 	font_size = 11,
